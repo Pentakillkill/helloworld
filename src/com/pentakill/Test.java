058b5fc9e2055fdaaddaaa1ee39e9ec40f966c3c
lijjai <lijjai@dcits.com>
2018-10-16 11:38:28 +0800

    厉害了

--- src/com/pentakill/Test.java
+++ src/com/pentakill/Test.java
@@ -4,9 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=10;
-		int b=20;
-		System.out.println(a+b);
+		int a = 10;
+		int b = 20;
+		System.out.println(a + b);
 		System.out.println("李谨进二笔。。。");
 		System.out.println("weiyikai   sb");
 		System.out.println("一颗卤蛋没了，晕+_+");
@@ -14,6 +14,7 @@ public class Test {
 		System.out.println("牛牛牛");
 		System.out.println("你好，请看一下！");
 		System.out.println("谨进，飞总，请看一下！！");
+		System.out.println("厉害了");
 	}
 
 }
